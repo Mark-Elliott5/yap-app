8a9dd6db5949756b4a6dadc9190f5152ec34530c
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-04-15 18:03:25 -0400

    Middleware and routes updates
    Users without usernames will get redirected to the onboarding page to
    add a username and displayName.

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,19 +1,40 @@
 import {
   apiAuthPrefix,
+  apiUploadPrefix,
   authRoutes,
   DEFAULT_LOGIN_REDIRECT,
+  DEFAULT_REGISTER_REDIRECT,
   publicRoutes,
 } from '@/src/routes';
 
-import { auth } from './auth';
+import { auth } from './app/api/auth/[...nextauth]/auth';
 
 export default auth((req) => {
   const { nextUrl } = req;
   const isLoggedIn = !!req.auth;
 
-  const isApiRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+  const isPublicRoute = publicRoutes.has(nextUrl.pathname);
+  const isUploadRoute = nextUrl.pathname.startsWith(apiUploadPrefix);
 
-  if (isApiRoute) return;
+  if (isApiAuthRoute || isPublicRoute) return;
+  if (isUploadRoute) return;
+
+  if (isLoggedIn) {
+    console.log(req.auth);
+    if (
+      !req.auth?.user.username &&
+      nextUrl.pathname !== DEFAULT_REGISTER_REDIRECT
+    ) {
+      return Response.redirect(new URL(DEFAULT_REGISTER_REDIRECT, nextUrl));
+    }
+    if (
+      nextUrl.pathname === DEFAULT_REGISTER_REDIRECT &&
+      req.auth?.user.username
+    ) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+  }
 
   const isAuthRoute = authRoutes.has(nextUrl.pathname);
 
@@ -24,7 +45,6 @@ export default auth((req) => {
     return;
   }
 
-  const isPublicRoute = publicRoutes.has(nextUrl.pathname);
   if (!isLoggedIn && !isPublicRoute) {
     return Response.redirect(new URL('/login', nextUrl));
   }
--- src/routes.ts
+++ src/routes.ts
@@ -13,10 +13,23 @@ const authRoutes = new Set(['/login', '/register', '/authentication-error']);
  * Prefix for authentication routes
  */
 const apiAuthPrefix = '/api/auth';
+const apiUploadPrefix = '/api/uploadthing';
 
 /**
  * Default login redirect path
  */
 const DEFAULT_LOGIN_REDIRECT = '/settings';
 
-export { apiAuthPrefix, authRoutes, DEFAULT_LOGIN_REDIRECT, publicRoutes };
+/**
+ * Default register redirect path to onboard user (get their desired username and displayName)
+ */
+const DEFAULT_REGISTER_REDIRECT = '/onboarding';
+
+export {
+  apiAuthPrefix,
+  apiUploadPrefix,
+  authRoutes,
+  DEFAULT_LOGIN_REDIRECT,
+  DEFAULT_REGISTER_REDIRECT,
+  publicRoutes,
+};
