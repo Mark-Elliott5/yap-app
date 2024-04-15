3548312ca5365af35a7d57430e1963db2448641b
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-04-15 18:09:26 -0400

    Preliminary UploadThing integration and files

--- src/@authcore.d.ts
+++
@@ -1,7 +0,0 @@
-// import { DefaultJWT } from '@auth/core/jwt';
-
-// declare module '@auth/core/jwt' {
-//   interface JWT extends Record<string, unknown>, DefaultJWT {
-//     role: 'ADMIN' | 'USER';
-//   }
-// }
