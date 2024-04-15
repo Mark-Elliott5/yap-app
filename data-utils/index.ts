b4a8e0847f18e75df52d2d05e3a49e8defa00e64
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-04-15 17:54:54 -0400

    Essential data utilities added

--- data-utils/index.ts
+++ data-utils/index.ts
@@ -1,4 +1,13 @@
+import { auth } from '@/src/app/api/auth/[...nextauth]/auth';
 import db from '@/src/lib/db';
+
+interface updateableFields {
+  email?: string;
+  password?: string;
+  avatar?: string;
+  displayName?: string;
+  username?: string;
+}
 
 const getUserByEmail = async (email: string) => {
   try {
@@ -8,7 +17,8 @@ const getUserByEmail = async (email: string) => {
       },
     });
     return user;
-  } catch {
+  } catch (err) {
+    console.log(err);
     return null;
   }
 };
@@ -21,9 +31,72 @@ const getUserById = async (id: string) => {
       },
     });
     return user;
-  } catch {
+  } catch (err) {
+    console.log(err);
     return null;
   }
 };
 
-export { getUserByEmail, getUserById };
+const getUserByUsername = async (username: string) => {
+  try {
+    const user = await db.user.findUnique({
+      where: {
+        username,
+      },
+    });
+    return user;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+const getCurrentUser = async () => {
+  try {
+    const session = await auth();
+    if (!session) return null;
+    const user = await db.user.findUnique({
+      where: {
+        id: session.user.id,
+      },
+    });
+    return user;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+// const getSession = async () => {
+//   try {
+//     const session = await auth();
+//     if (!session) return null;
+//     return session;
+//   } catch (err) {
+//     console.log(err);
+//     return null;
+//   }
+// };
+
+const updateUser = async (id: string, data: updateableFields) => {
+  try {
+    const user = await db.user.update({
+      where: {
+        id,
+      },
+      data,
+    });
+    return user;
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+};
+
+export {
+  getCurrentUser,
+  getUserByEmail,
+  getUserById,
+  getUserByUsername,
+  updateUser,
+};
