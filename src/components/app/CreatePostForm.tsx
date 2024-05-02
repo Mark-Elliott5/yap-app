8935b6fb7e634922f3cc6b6b4e374cc61f120829
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-05-02 14:38:20 -0400

    CreatePostForm
    Split off from CreatePostButton

---
+++ src/components/app/CreatePostForm.tsx
@@ -0,0 +1,109 @@
+import { useEffect, useState } from 'react';
+import { Form, useForm } from 'react-hook-form';
+import { z } from 'zod';
+
+import { createPost } from '@/actions/actions';
+import { CreatePostSchema } from '@/schemas';
+import FormButton from '@/src/components/FormButton';
+import FormError from '@/src/components/FormError';
+import FormSuccess from '@/src/components/FormSuccess';
+import {
+  Form as FormProvider,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/src/components/ui/form';
+import { Input } from '@/src/components/ui/input';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+function CreatePostForm() {
+  const [changeTry, setChangeTry] = useState<{
+    error?: string;
+    success?: string;
+  }>({});
+  const form = useForm<z.infer<typeof CreatePostSchema>>({
+    resolver: zodResolver(CreatePostSchema),
+  });
+  const { isSubmitting } = form.formState;
+
+  const handleChange = async (data: FormData) => {
+    setChangeTry(await createPost(data));
+  };
+
+  useEffect(() => {
+    if (changeTry.success) {
+      const timeout = setTimeout(() => {
+        setChangeTry({});
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [changeTry]);
+
+  return (
+    <FormProvider {...form}>
+      <Form
+        // action={createPost}
+        onSubmit={({ formData }) => handleChange(formData)}
+        className='margin-auto self-center'
+      >
+        <div className='flex flex-col gap-2 pb-6'>
+          <FormField
+            // control={form.control}
+            name='text'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Text</FormLabel>
+                <FormControl>
+                  <Input
+                    {...field}
+                    placeholder='Yappity yap'
+                    type='text'
+                    minLength={0}
+                    maxLength={32}
+                    className='placeholder:italic'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            // unnecessary when using FormProvider according to docs
+            // control={form.control}
+            name='image'
+            render={({ field: { value, onChange, ...fieldProps } }) => (
+              <FormItem>
+                <FormLabel>Image</FormLabel>
+                <FormControl>
+                  <Input
+                    {...fieldProps}
+                    placeholder='Image'
+                    type='file'
+                    accept='image/jpeg, image/png, image/jpg'
+                    onChange={(event) =>
+                      onChange(event.target.files && event.target.files[0])
+                    }
+                    className='text-zinc-950 dark:text-zinc-100 dark:placeholder:text-zinc-950'
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+        <div className='flex flex-col gap-y-6'>
+          {changeTry?.error && <FormError message={changeTry.error} />}
+          {changeTry?.success && <FormSuccess message={changeTry.success} />}
+          <FormButton disabled={isSubmitting}>
+            {isSubmitting ? 'Posting...' : 'Post'}
+          </FormButton>
+        </div>
+      </Form>
+    </FormProvider>
+  );
+}
+
+export default CreatePostForm;
