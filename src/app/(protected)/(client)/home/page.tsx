7c69e8a172a3120820b65fc4828aa267e9b49309
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-05-07 22:38:02 -0400

    Feat: Add Latest and Following link to /home

--- src/app/(protected)/(client)/home/page.tsx
+++ src/app/(protected)/(client)/home/page.tsx
@@ -1,18 +1,36 @@
+import Link from 'next/link';
+
 import YapPost from '@/src/components/yap/YapPost';
 import { getLatestYaps } from '@/src/lib/database/fetch';
 import { getCurrentUsername } from '@/src/lib/database/getUser';
 
 async function Home() {
-  // get latest yaps, return fragment of yaps
-  // return <></>;
   const currentUsername = await getCurrentUsername();
   if (!currentUsername) return null;
 
   const { yaps, error } = await getLatestYaps();
   if (yaps && yaps.length) {
-    return yaps.map((yap) => (
-      <YapPost key={yap.id} currentUsername={currentUsername} {...yap} />
-    ));
+    return (
+      <>
+        <div className='mt-4 flex gap-4 text-xl text-zinc-950 dark:text-zinc-100'>
+          <Link
+            href='/home'
+            className='rounded-md bg-white px-4 py-2 shadow-lg transition-all hover:scale-[1.2] dark:bg-zinc-900'
+          >
+            Latest
+          </Link>
+          <Link
+            href='/following'
+            className='px-4 py-2 transition-all hover:scale-[1.2] hover:opacity-70'
+          >
+            Following
+          </Link>
+        </div>
+        {yaps.map((yap) => (
+          <YapPost key={yap.id} currentUsername={currentUsername} {...yap} />
+        ))}
+      </>
+    );
   }
 
   if (error) {
