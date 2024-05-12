a4f32ddc40da0b12363ad012ac67ff08f25505a8
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-05-12 03:19:49 -0400

    Fix: Removing old components causing build errors

--- src/components/yap/ReplyButton.tsx
+++ src/components/yap/ReplyButton.tsx
@@ -50,7 +50,6 @@ function ReplyButton({
           </TooltipContent>
         </Tooltip>
       </TooltipProvider>
-      {/* {postVisible && <PostPopup setPostVisible={setPostVisible} id={id} />} */}
     </>
   );
 }
