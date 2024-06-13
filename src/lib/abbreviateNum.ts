e32bacad0fe4bee6358aae828c5df4bf6bbf4722
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-06-13 10:45:16 -0400

    Update abbreviateNum to use Intl built-in object

--- src/lib/abbreviateNum.ts
+++ src/lib/abbreviateNum.ts
@@ -1,29 +1,15 @@
-/** Convert a number greater than 4 digits long into an abbreviated string.
- * @param {number} num The number to be converted.
- * @returns {string} The formatted string.
- *
- * Examples:
- *
- * abbreviateNum(1234) => 1.2K
- *
- * abbreviateNum(9801304) => 9.8M
- *
- * abbreviateNum(455) => 455
- * */
-function abbreviateNum(num: number): string {
-  const iterations = recur(num);
-  if (iterations === 0) {
-    return num.toString();
-  }
-  const letterArr = ['K', 'M', 'B'];
-  return `${(num / 1000 ** iterations).toFixed(1)}${letterArr[iterations > 3 ? 2 : iterations - 1]}`;
-}
+'use client';
 
-function recur(num: number): number {
-  if (num < 999) {
-    return 0;
-  }
-  return 1 + recur(num / 1000);
+/**
+ * Convert number or BigInt to compact form per user locale, e.g. 1452 -> 1.5K.
+ */
+function abbreviateNum(num: number | bigint) {
+  if (typeof num !== 'number' && typeof num !== 'bigint') return;
+  const abbreviation = new Intl.NumberFormat(undefined, {
+    notation: 'compact',
+  }).format(num);
+
+  return abbreviation;
 }
 
 export default abbreviateNum;
