244f7ee4d274a0fed051bfdd7b58c8d90ae0ef5e
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2024-07-06 13:09:42 -0400

    Fix: Mentioning non-existent users

--- src/components/yap/AutoMention.tsx
+++ src/components/yap/AutoMention.tsx
@@ -1,16 +1,23 @@
 import Link from 'next/link';
 import reactStringReplace from 'react-string-replace';
 
+import { getUserByUsername } from '@/src/lib/database/getUser';
+
 function AutoMention({ text }: { text: string }) {
-  const links = reactStringReplace(text, /@(\w+)/g, (match, i) => (
-    <Link
-      key={match + i}
-      href={`/user/${match}`}
-      className='text-yap-blue-500 hover:text-yap-blue-600'
-    >
-      @{match}
-    </Link>
-  ));
+  const links = reactStringReplace(text, /@(\w+)/g, async (match, i) => {
+    const matchExists = await getUserByUsername(match);
+    return matchExists ? (
+      <Link
+        key={match + i}
+        href={`/user/${match}`}
+        className='text-yap-blue-500 hover:text-yap-blue-600'
+      >
+        @{match}
+      </Link>
+    ) : (
+      <>@{match}</>
+    );
+  });
 
   return <>{links.map((link) => link)}</>;
 }
