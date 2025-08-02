b239eb9c672c1cf68068767a91abfcffe5230c18
Mark Elliott <115184056+Mark-Elliott5@users.noreply.github.com>
2025-08-02 10:31:00 -0400

    Update SomethingWentWrong for DRY

--- src/components/yap/SomethingWentWrong.tsx
+++ src/components/yap/SomethingWentWrong.tsx
@@ -1,10 +1,10 @@
+import ListElement from '@/src/components/yap/ListElement';
+
 function SomethingWentWrong() {
   return (
-    <span
-      className={`flex w-full flex-col gap-2 rounded-lg border-x-[0.5px] border-t-1 border-zinc-200 bg-white px-5 py-4 text-center text-sm italic shadow-xl sm:text-base dark:border-zinc-800 dark:bg-zinc-900`}
-    >
+    <ListElement className='text-center italic'>
       Something went wrong! Try again.
-    </span>
+    </ListElement>
   );
 }
 
